4d0f04862338706910e2d20721386e19085876be
designerajayk <ak472526@gmail.com>
2017-01-22 13:08:02 -0500

    submit form on console log

--- client/components/SignupPage/index.js
+++ client/components/SignupPage/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import map from 'lodash/map';
+
+
+import countrys from '../../data/country';
+
 import {
     FormGroup,
     ControlLabel,
@@ -13,39 +18,68 @@ import {
 } from 'react-bootstrap';
 
 class SignupPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      username: '',
+      email: '',
+      password: '',
+      passwordCon: '',
+      country: '',
+      message: ''
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
+
+  onChange(e){
+    this.setState({[e.target.name]: e.target.value})
+  }
+  onSubmit(e){
+    e.preventDefault();
+    console.log(this.state)
+  }
   render(){
-    function FieldGroup({id, label, help, text}) {
-            return (
-                <FormGroup controlId={id}>
-                    <ControlLabel>{label}</ControlLabel>
-                    <FormControl type={text}/> {help && <HelpBlock>{help}</HelpBlock>}
-                </FormGroup>
-            );
-        }
+const options = map(countrys, (val, key) =>
+  <option key={val} value={val}>{key}</option>
+)
     return(
       <div className="container">
-
               <Row>
                   <Col sm={6} smOffset={3}>
+                      <form onSubmit={this.onSubmit}>
+                        <FormGroup controlId="username">
+                            <ControlLabel>User Name</ControlLabel>
+                            <FormControl type="text" name="username" value={this.state.username} onChange={this.onChange} />
+                        </FormGroup>
 
-                      <form>
-                          <FieldGroup id="username" type="text" label="User Name" placeholder="Enter text"/>
-                          <FieldGroup id="email" type="email" label="Email address" placeholder="Enter email"/>
-                          <FieldGroup id="password" label="Password" type="password"/>
-                          <FieldGroup id="passwordCon" label="Password Conformation" type="password"/>
+                        <FormGroup controlId="email">
+                            <ControlLabel>Email address</ControlLabel>
+                            <FormControl type="email" name="email" value={this.state.email} onChange={this.onChange} />
+                        </FormGroup>
 
+                        <FormGroup controlId="password">
+                            <ControlLabel>Password</ControlLabel>
+                            <FormControl type="password" name="password" value={this.state.password} onChange={this.onChange} />
+                        </FormGroup>
 
-                          <FormGroup controlId="formControlsSelect">
+                        <FormGroup controlId="passwordCon">
+                            <ControlLabel>Password Conformation</ControlLabel>
+                            <FormControl type="password" name="passwordCon" value={this.state.passwordCon} onChange={this.onChange} />
+                        </FormGroup>
+
+                          <FormGroup controlId="country">
                               <ControlLabel>Contry</ControlLabel>
-                              <FormControl componentClass="select" placeholder="select">
+                              <FormControl name="country" componentClass="select" placeholder="select" value={this.state.country} onChange={this.onChange}>
                                   <option value="select" disabled>select</option>
-                                  <option value="other">...</option>
+                                  {options}
                               </FormControl>
                           </FormGroup>
 
-                          <FormGroup controlId="formControlsTextarea">
-                              <ControlLabel>Textarea</ControlLabel>
-                              <FormControl componentClass="textarea" placeholder="textarea"/>
+                          <FormGroup controlId="message">
+                              <ControlLabel>Message</ControlLabel>
+                              <FormControl name="message" componentClass="textarea" value={this.state.message} onChange={this.onChange} placeholder="textarea"/>
                           </FormGroup>
 
                           <FormGroup>
---
+++ client/data/country.js
@@ -0,0 +1,5 @@
+export default {
+  "Delhi": "delhi",
+  "India": "india",
+  "Kolkata": "kolkata"
+}
