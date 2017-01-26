66040722311632463a15d675bfe661831f3ead62
designerajayk <ak472526@gmail.com>
2017-01-26 19:17:29 +0530

    add server side validation with react-bootstrap

--- client/components/SignupForm/index.js
+++ client/components/SignupForm/index.js
@@ -1,91 +1,96 @@
 import React from 'react';
 import map from 'lodash/map';
 
-
 import countrys from '../../data/country';
 
-import {
-    FormGroup,
-    ControlLabel,
-    HelpBlock,
-    FormControl,
-    Button,
-} from 'react-bootstrap';
+import {FormGroup, ControlLabel, HelpBlock, FormControl, Button} from 'react-bootstrap';
 
 class SignupForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state ={
-      username: '',
-      email: '',
-      password: '',
-      passwordCon: '',
-      country: '',
-      message: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            passwordCon: '',
+            country: '',
+            message: '',
+            errors: {}
+        }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
     }
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
 
-  }
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    onSubmit(e) {
 
-  onChange(e){
-    this.setState({[e.target.name]: e.target.value})
-  }
-  onSubmit(e){
-    e.preventDefault();
-    this.props.userSign(this.state);
-  }
-  render(){
-const options = map(countrys, (val, key) =>
-  <option key={val} value={val}>{key}</option>
-)
-    return(
-                      <form onSubmit={this.onSubmit}>
-                        <FormGroup controlId="username" validationState={'success'}>
-                            <ControlLabel>User Name</ControlLabel>
-                            <FormControl type="text" name="username" value={this.state.username} onChange={this.onChange}  />
-                            <FormControl.Feedback />
-                        </FormGroup>
+        this.setState({errors: {}})
+        e.preventDefault();
+        this.props.userSign(this.state).then(
+()=>{},
+({response})=>{this.setState({errors:response.data})}
+);﻿
+    }
+    render() {
+        const {errors} = this.state;
+        const options = map(countrys, (val, key) => <option key={val} value={val}>{key}</option>
+        )
+        return (
+            <form onSubmit={this.onSubmit}>
+                <FormGroup controlId="username" validationState={errors.username}>
+                    <ControlLabel>User Name</ControlLabel>
+                    <FormControl type="text" name="username" value={this.state.username} onChange={this.onChange}/>
+                    <FormControl.Feedback/>
+                </FormGroup>
 
-                        <FormGroup controlId="email">
-                            <ControlLabel>Email address</ControlLabel>
-                            <FormControl type="email" name="email" value={this.state.email} onChange={this.onChange} />
-                        </FormGroup>
+                <FormGroup controlId="email" validationState={errors.email}>
+                    <ControlLabel>Email address</ControlLabel>
+                    <FormControl type="text" name="email" value={this.state.email} onChange={this.onChange}/>
+                    <FormControl.Feedback/>
+                </FormGroup>
 
-                        <FormGroup controlId="password">
-                            <ControlLabel>Password</ControlLabel>
-                            <FormControl type="password" name="password" value={this.state.password} onChange={this.onChange} />
-                        </FormGroup>
+                <FormGroup controlId="password" validationState={errors.password}>
+                    <ControlLabel>Password</ControlLabel>
+                    <FormControl type="password" name="password" value={this.state.password} onChange={this.onChange}/>
+                    <FormControl.Feedback/>
+                </FormGroup>
 
-                        <FormGroup controlId="passwordCon">
-                            <ControlLabel>Password Conformation</ControlLabel>
-                            <FormControl type="password" name="passwordCon" value={this.state.passwordCon} onChange={this.onChange} />
-                        </FormGroup>
+                <FormGroup controlId="passwordCon" validationState={errors.passwordCon}>
+                    <ControlLabel>Password Conformation</ControlLabel>
+                    <FormControl type="password" name="passwordCon" value={this.state.passwordCon} onChange={this.onChange}/>
+                    <FormControl.Feedback/>
+                </FormGroup>
 
-                          <FormGroup controlId="country">
-                              <ControlLabel>Contry</ControlLabel>
-                              <FormControl name="country" componentClass="select" placeholder="select" value={this.state.country} onChange={this.onChange}>
-                                  <option value="select" disabled>select</option>
-                                  {options}
-                              </FormControl>
-                          </FormGroup>
+                <FormGroup controlId="country" validationState={errors.country}>
+                    <ControlLabel>Contry</ControlLabel>
+                    <FormControl name="country" componentClass="select" placeholder="select" value={this.state.country} onChange={this.onChange}>
+                        <option value="select" disabled>select</option>
+                        {options}
+                    </FormControl>
+                    <FormControl.Feedback/>
+                </FormGroup>
 
-                          <FormGroup controlId="message">
-                              <ControlLabel>Message</ControlLabel>
-                              <FormControl name="message" componentClass="textarea" value={this.state.message} onChange={this.onChange} placeholder="textarea"/>
-                          </FormGroup>
+                <FormGroup controlId="message" validationState={errors.message}>
+                    <ControlLabel>Message</ControlLabel>
+                    <FormControl name="message" componentClass="textarea" value={this.state.message} onChange={this.onChange} placeholder="textarea"/>
+                    <FormControl.Feedback/>
+                </FormGroup>
 
-                          <FormGroup>
-                            <Button type="submit" bsStyle="primary" bsSize="large" block>
-                                Submit
-                            </Button>
-                         </FormGroup>
-                      </form>
+                <FormGroup>
+                    <Button type="submit" bsStyle="primary" bsSize="large" block>
+                        Submit
+                    </Button>
+                </FormGroup>
+            </form>
 
-    );
+        );
 
-  }
+    }
 }
-
 
 export default SignupForm;
---
+++ server/routes/users.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import Validator from 'validator';
+import isEmpty from 'lodash/isEmpty';
+
+let router = express.Router();
+
+let allEqual = arr => arr.every(v => v == arr[0]);
+
+function validateInput(data) {
+    let errors = {};
+
+    if (Validator.isEmpty(data.username)) {
+        errors.username = 'warning';
+    } else {
+        errors.username = 'success';
+    }
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'error';
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = 'warning';
+    } else {
+        errors.email = 'success';
+    }
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'error';
+    } else {
+        errors.password = 'success';
+    }
+
+    if (Validator.isEmpty(data.passwordCon)) {
+        errors.passwordCon = 'error'
+    } else if (!Validator.equals(data.password, data.passwordCon)) {
+        errors.passwordCon = 'warning';
+    } else {
+        errors.passwordCon = 'success';
+    }
+
+    if (Validator.isEmpty(data.country)) {
+        errors.country = 'error'
+    } else {
+        errors.country = 'success'
+    }
+
+    if (Validator.isEmpty(data.message)) {
+        errors.message = 'error'
+    } else {
+        errors.message = 'success'
+    }
+
+    return {
+        errors, isValid: allEqual(Object.values(errors))
+    }
+}
+
+router.post('/', (req, res) => {
+    const {errors, isValid} = validateInput(req.body);
+
+    if (!isValid) {
+        res.status(400).json(errors);
+
+    }
+})
+
+export default router;
